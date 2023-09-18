000f6140af61b62fbc99fea9cf3c901ad6f0e9e1
shahnewazIbrahim <shahnewaz886@gmail.com>
2023-09-18 17:08:14 +0600

    sdfdsf

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,34 @@ import ReactDOM from 'react-dom/client';
 
 // const element = React.createElement("h1", {className='heading'}, "hello world");
 
-  const element = (
-    <h1 className='heading'>
-      <span>
-        Hello {new Date().toLocaleTimeString()}
-      </span>
-    </h1>
-  );
+// component that return element 
+// function Clock({locale}) {
+//   return (
+//     <h1 className='heading'>
+//       <span>
+//         Hello {new Date().toLocaleTimeString(locale)}
+//       </span>
+//     </h1>
+//   );
+// }
+
+class Clock extends React.Component{
+  print() {
+    return (
+      <h1 className='heading'>
+        <span>
+          Hello {new Date().toLocaleTimeString()}
+        </span>
+      </h1>
+    );
+  }
+}
   
-// ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(<Clock/>, document.getElementById('root'));
+
+const clockComponent = new Clock();
   
-  ReactDOM.createRoot(document.getElementById('root')).render( 
-  element
-)
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(clockComponent.print())
   
