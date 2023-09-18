6ec88a7b78feb9eafe2776698fb67e38a4fa4fc4
shahnewazIbrahim <shahnewaz886@gmail.com>
2023-09-18 16:39:21 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+// const element = React.createElement("h1", {className='heading'}, "hello world");
+
+  const element = (
+    <h1 className='heading'>
+      <span>
+        Hello {new Date().toLocaleTimeString()}
+      </span>
+    </h1>
+  );
+  
+// ReactDOM.render(element, document.getElementById('root'));
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+  element
+)
+  
