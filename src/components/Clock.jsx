4946c0793500db7e9c9c8e3cfc2cd371762b57f5
shahnewazIbrahim <shahnewaz886@gmail.com>
2023-09-19 15:49:11 +0600

    state

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class Clock extends React.Component{
-    // state define smart way //
+    // state define shorthand way //
     state = {
         date : new Date()
     };
